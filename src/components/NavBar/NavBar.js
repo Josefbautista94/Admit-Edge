e18f6241c683240376ea80dc5a44c218b59ac225
Jose <josefbautista94@gmail.com>
2023-12-26 23:44:37 -0500

    created a Navbar to start with

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './NavBar.css'; // Ensure this path is correct
+import logo from '../Images/logo.jpeg';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <a href="#" className="navbar-logo-container">
+        <img src={logo} alt="Logo" className="navbar-logo" />
+      </a>
+      <div className="nav-logo">
+        <a href="#">
+          <span>Admit</span>
+          <span>Edge</span>
+        </a>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Services</a>
+          <div className="sub-navbar">
+            {/* Content of your smaller navbar */}
+            <a href="#">Sub Link 1</a>
+            <a href="#">Sub Link 2</a>
+            {/* ... more links ... */}
+          </div></li>
+        <li><a href="#">College Acceptances</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+
+    </nav>
+  );
+};
+
+export default NavBar;
