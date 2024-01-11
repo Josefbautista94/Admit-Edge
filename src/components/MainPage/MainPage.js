4f8dd40e8830d4ceced1f614a288ec7365eee7d7
Jose <josefbautista94@gmail.com>
2024-01-10 23:19:26 -0500

    Installed react router

---
+++ src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './MainPage.css'; // Ensure this path is correct
+
+const MainPage = () => {
+  return (
+    <div className="mainContainer">
+    
+    </div>
+  );
+};
+
+export default MainPage;
