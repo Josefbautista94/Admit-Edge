acb367da0e4dfc2897d8037ebf337643afa190ec
Jose <josefbautista94@gmail.com>
2024-01-12 03:51:29 -0500

    added a background and future review section to the hompage

--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -1,11 +1,22 @@
-import React from 'react';
-import './MainPage.css';
+import React from "react";
+import "./MainPage.css";
 
 const MainPage = () => {
   return (
     <div className="mainContainer">
- <h1 className='title'>Admit Edge</h1>
-      </div>
+      <header className="titleHeader">
+        <h1 className="title">Admit Edge</h1>
+        <h2 className="prestige-admissions-consulting">
+          Admit Edge Consulting stands at the forefront of university admissions
+          expertise. We are dedicated to nurturing students' academic growth and
+          crafting strategic pathways for their successful entry into
+          prestigious colleges.
+        </h2>
+      </header>
+      <div class="review-container">
+        <div className = "reviews"> </div>
+        </div>{" "}
+    </div>
   );
 };
 
