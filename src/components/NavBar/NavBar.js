b0233d9b0d55d2fbd5ec1b6397ef3d648f57ea71
Jose <josefbautista94@gmail.com>
2024-02-09 01:40:11 -0500

    Add direct links to sections in CollegeAcceptances from NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,8 +1,7 @@
 import React from "react";
 import "./NavBar.css"; // Ensure this path is correct
 import logo from "../Images/logo.jpeg";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
@@ -12,22 +11,21 @@ const NavBar = () => {
       </a>
       <div className="nav-logo">
         <a href="#">
-          <span className = "ae">Admit</span>
-          <span className = "ae">Edge</span>
+          <span className="ae">Admit</span>
+          <span className="ae">Edge</span>
         </a>
       </div>
       <ul className="nav-links">
         <li>
-        <Link to="/Home">Home</Link>
+          <Link to="/Home">Home</Link>
           <div className="sub-navbar">
-           
             <a href="#">Reviews</a>
             {/* ... more links ... */}
           </div>
         </li>
 
         <li>
-        <Link to="/Services">Services</Link>
+          <Link to="/Services">Services</Link>
 
           {/* Sub-navbar is now a sibling to the link, not a child */}
           <div className="sub-navbar">
@@ -39,17 +37,22 @@ const NavBar = () => {
           </div>
         </li>
         <li>
-        <Link to="/College-Acceptances">College Acceptances</Link>
-          <div className="sub-navbar">
-            <a href="#">Top Ranked Colleges</a>
-            <a href="#">Undergraduate Business Schools</a>
-            <a href="#">Complete List
-</a>
+          <Link to="/College-Acceptances">College Acceptances</Link>
+          <ul className="sub-navbar"> {/* Changed from div to ul for semantic correctness */}
+            <li>
+              <Link to="/College-Acceptances#top-ranked-colleges">Top Ranked Colleges</Link>
+            </li>
+            <li>
+              <Link to="/College-Acceptances#undergraduate-business-schools">Undergraduate Business Schools</Link>
+            </li>
+            <li>
+              <Link to="/College-Acceptances#complete-list">Complete List</Link>
+            </li>
             {/* ... more links ... */}
-          </div>
+          </ul> {/* Closing tag for ul */}
         </li>
         <li>
-        <Link to="/About">About</Link>
+          <Link to="/About">About</Link>
           <div className="sub-navbar">
             <a href="#">Sub Link 1</a>
             <a href="#">Sub Link 2</a>
@@ -57,7 +60,7 @@ const NavBar = () => {
           </div>
         </li>
         <li>
-        <Link to="/Contact">Contact</Link>
+          <Link to="/Contact">Contact</Link>
           <div className="sub-navbar">
             <a href="#">Sub Link 1</a>
             <a href="#">Sub Link 2</a>
